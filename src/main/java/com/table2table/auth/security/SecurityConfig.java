328f5034336c28db0d6487e5a4292f95ae1bc157
rxnath <ramnathkizhutil1408@gmail.com>
2025-07-20 16:04:23 +0530

    Implemented auth APIs

--- src/main/java/com/table2table/auth/security/SecurityConfig.java
+++ src/main/java/com/table2table/auth/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
